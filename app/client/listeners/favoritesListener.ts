289745b20a299a98fb711fb3eedc5868a6c804f3
chachopacho <bordonnet@hotmail.com>
2021-10-15 13:49:51 -0300

    Listen to API

--- app/client/listeners/favoritesListener.ts
+++ app/client/listeners/favoritesListener.ts
@@ -10,6 +10,9 @@ const SEARCH_INPUT_S_EVENT: string = "keyup";
 export default function favoritesListener(): void {
   Array.from(FAVORITES_ICONS).forEach((favorites_icon: Element): void => {
     favorites_icon.addEventListener(SEARCH_INPUT_S_EVENT, (e) => {
+      if (!app.users.isLogued()) {
+        return alert("Necesitas estar Logueado!");
+      }
       const productID: number = +(favorites_icon as HTMLElement).dataset.id;
       app.favorites.handleFavorite(productID);
     });
