fe8ebdeadf5a61a33388df9aa5fad3a6bcd44f44
chachopacho <bordonnet@hotmail.com>
2021-10-20 09:54:17 -0300

    Config

--- app/server/app.ts
+++ app/server/app.ts
@@ -7,8 +7,6 @@ const { PUBLIC_DIR } = require('./config');
 
 const app = express();
 
-// ["Primeros auxilios", "Diabetes", "Óptica", "Pediculosis", "Pédicos", "Suplementos Dietarios", "Suplementos Nutricionales", "Suplementos Deportivos", "Protección y Recuperación", "Fitness"];
-
 // settings
 app.set('view engine', 'ejs');
 app.listen(4444);
--- app/server/config.ts
+++ app/server/config.ts
@@ -1,12 +1,11 @@
-const path = require('path');
+const path = require("path");
 
-const ROOT_PATH = [__dirname, '..', '..'];
+const ROOT_PATH = [__dirname, "..", ".."];
 
 const ROOT = path.join(...ROOT_PATH);
-const PUBLIC_DIR = path.join(...ROOT_PATH, 'public');
-const DATABASE_DIR = path.join(...ROOT_PATH, 'database');
+const PUBLIC_DIR = path.join(...ROOT_PATH, "public");
+const DATABASE_DIR = path.join(...ROOT_PATH, "database");
 
-export {
-  PUBLIC_DIR,
-  DATABASE_DIR
-}
+const TABLES = ["config", "products", "users", "favorites", "categories"];
+
+export { PUBLIC_DIR, DATABASE_DIR, TABLES };
