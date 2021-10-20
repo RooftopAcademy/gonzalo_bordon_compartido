c0eb980cb926d54cff50a18cd4b320164502abb2
chachopacho <bordonnet@hotmail.com>
2021-10-20 09:53:56 -0300

    Query Parser

--- app/client/class/Router.ts
+++ app/client/class/Router.ts
@@ -11,6 +11,10 @@ export default class Router {
     );
   }
 
+  public static followWithStringQuery(path: string, query: string): void {
+    window.location.replace(window.location.origin + path + query);
+  }
+
   public static followWithCurrentQuery(path: string): void {
     window.location.replace(window.location.origin + path + Router.getQuery());
   }
@@ -31,12 +35,42 @@ export default class Router {
     return window.location.pathname.split("/")[nParam + 1];
   }
 
+  public static parseQuery(): any {
+    const query: any = {};
+    window.location.search
+      .replace("?", "")
+      .split("&")
+      .forEach((entry) => {
+        const pair: string[] = entry.split("=");
+        if (query[pair[0]]) {
+          if (query[pair[0]] instanceof Array) {
+            return query[pair[0]].push(pair[1]);
+          }
+          return (query[pair[0]] = [query[pair[0]], pair[1]]);
+        }
+        query[pair[0]] = pair[1];
+      });
+
+    return query;
+  }
+
   private static stringfyQuery(query: any): string {
-    return (
-      "?" +
-      Object.keys(query)
-        .map((key) => `${key}=${query[key]}`)
-        .join("&")
-    );
+    return "?" + Router.appendStringfyQuery(query);
+  }
+
+  private static appendStringfyQuery(query: any): string {
+    return Object.keys(query)
+      .filter(
+        (key) =>
+          Boolean(query[key]) &&
+          (query[key] instanceof Array ? query[key].length !== 0 : true)
+      )
+      .map((key) => {
+        console.log(key, query[key]);
+        return query[key] instanceof Array
+          ? (query[key] as string[]).map((value) => `${key}=${value}`).join("&")
+          : `${key}=${query[key]}`;
+      })
+      .join("&");
   }
 }
