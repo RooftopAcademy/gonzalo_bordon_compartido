2480580d56b08bcb234c583e7a56bc1779708464
chachopacho <bordonnet@hotmail.com>
2021-10-20 05:34:06 -0300

    View for empty products list

---
+++ app/server/components/ProductsEmptyComponent.ts
@@ -0,0 +1,9 @@
+export default function ProductsEmptyComponent(): string {
+  return /* html */ `
+    <div class="product-card">
+      <div class="product-info">
+        <h5><a href="/products">No se han encontrado productos!</a></h5>
+      </div>
+    </div>
+    `;
+}
