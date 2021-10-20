4c22a06dc439bfde804c17d8193f639ab9735f9c
chachopacho <bordonnet@hotmail.com>
2021-10-20 05:36:00 -0300

    Rename and split in two versions

---
+++ app/client/listeners/favoritesAsyncListener.ts
@@ -0,0 +1,35 @@
+import Favorites from "../class/Favorites";
+import Users from "../class/Users";
+
+//  HTML
+const FAVORITES_ICONS: HTMLCollectionOf<Element> =
+  document.getElementsByClassName("favorites-icon");
+
+//  EVENTS
+const FAVORITES_ICON_EVENT: string = "click";
+
+export default async function favoritesAsyncListener(
+  users: Users,
+  favorites: Favorites
+): Promise<void> {
+  const favoritesArray = await favorites.getFavoritesList();
+  const isLogued: boolean = users.isLogued();
+
+  Array.from(FAVORITES_ICONS).forEach((favorites_icon: Element): void => {
+    const favoriteID: number = +(favorites_icon as HTMLElement).dataset.id;
+
+    if (favoritesArray.includes(`${favoriteID}`)) {
+      favorites_icon.classList.add("selected");
+    }
+
+    favorites_icon.addEventListener(FAVORITES_ICON_EVENT, () => {
+      if (!isLogued) {
+        return alert("Necesitas estar Logueado!");
+      }
+      const productID: number = +(favorites_icon as HTMLElement).dataset.id;
+      favorites.toggleFavorites(`${productID}`);
+
+      favorites_icon.classList.toggle("selected");
+    });
+  });
+}
--- app/client/listeners/favoritesSyncListener.ts
+++ app/client/listeners/favoritesSyncListener.ts
@@ -1,5 +1,4 @@
 import Favorites from "../class/Favorites";
-import Users from "../class/Users";
 
 //  HTML
 const FAVORITES_ICONS: HTMLCollectionOf<Element> =
@@ -8,24 +7,13 @@ const FAVORITES_ICONS: HTMLCollectionOf<Element> =
 //  EVENTS
 const FAVORITES_ICON_EVENT: string = "click";
 
-export default async function favoritesListener(
-  users: Users,
+export default function favoritesSyncListener(
   favorites: Favorites
-): Promise<void> {
-  const favoritesArray = await favorites.getFavorites();
-  const isLogued: boolean = users.isLogued();
-
+): void {
   Array.from(FAVORITES_ICONS).forEach((favorites_icon: Element): void => {
     const favoriteID: number = +(favorites_icon as HTMLElement).dataset.id;
 
-    if (favoritesArray.includes(`${favoriteID}`)) {
-      favorites_icon.classList.add("selected");
-    }
-
     favorites_icon.addEventListener(FAVORITES_ICON_EVENT, () => {
-      if (!isLogued) {
-        return alert("Necesitas estar Logueado!");
-      }
       const productID: number = +(favorites_icon as HTMLElement).dataset.id;
       favorites.toggleFavorites(`${productID}`);
 
